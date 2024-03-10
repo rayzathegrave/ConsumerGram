80c02243ec39ddc1b383b0682a6b2ff8250a4650
rayzathegrave <rdgraaf213@gmail.com>
2024-03-10 19:52:38 +0100

    applicatie afgemaakt nu laatste loodjes en alles zorgvuldig nakijken

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -4,6 +4,12 @@ import {useContext, useState} from "react";
 import SatisfiedConsumersButton from "../satisfiedButton/SatisfiedConsumersButton.jsx";
 import NotSatisfiedConsumersButton from "../notsatisfiedConsumersButton/NotSatisfiedConsumersButton.jsx";
 import UndoButton from "../undoButton/UndoButton.jsx";
+import shirtIcon from "./../../assets/tshirt-Icon.png";
+import CarIcon from "./../../assets/Car-Icon.png";
+import House from "./../../assets/House-Icon.png";
+import Tech from "./../../assets/Tech-Icon.png";
+import Decoration from "./../../assets/Decoration-Icon.png";
+import Other from "./../../assets/Other-Icon.png";
 
 function Categories() {
     const { setSearchQuery } = useContext(SearchContext);
@@ -32,15 +38,16 @@ function Categories() {
         <>
 
 
-            <ul className="catlist">
+                <ul className="catlist1">
                 <h3>Categories</h3>
-                <li onClick={() => handleCategoryClick('Cars')} className="categoryItem">Cars</li>
-                <li onClick={() => handleCategoryClick('Tech')} className="categoryItem">Tech</li>
-                <li onClick={() => handleCategoryClick('Decoration')} className="categoryItem">Decoration</li>
-                <li onClick={() => handleCategoryClick('Clothes')} className="categoryItem">Clothes</li>
-                <li onClick={() => handleCategoryClick('Houses')} className="categoryItem">Houses</li>
-                <li onClick={() => handleCategoryClick('Other')} className="categoryItem">Other</li>
-
+                    <div className="categoryRowContainer"><img src={CarIcon} alt="CarIcon"/><li onClick={() => handleCategoryClick('Cars')} className="categoryItem">Cars</li></div>
+                    <div className="categoryRowContainer"><img src={Tech} alt="Tech"/><li onClick={() => handleCategoryClick('Tech')} className="categoryItem">Tech</li></div>
+                        <div className="categoryRowContainer"><img src={Decoration} alt="Decoration icon"/><li onClick={() => handleCategoryClick('Decoration')} className="categoryItem">Decoration</li></div>
+                            <div className="categoryRowContainer"><img src={shirtIcon} alt="Shirt icon"/><li onClick={() => handleCategoryClick('Clothes')} className="categoryItem">Clothes</li></div>
+                                <div className="categoryRowContainer"><img src={House} alt="Home icon"/><li onClick={() => handleCategoryClick('Houses')} className="categoryItem">Houses</li></div>
+                                    <div className="categoryRowContainer"><img src={Other} alt="Other icon"/><li onClick={() => handleCategoryClick('Other')} className="categoryItem">Other</li></div>
+                </ul>
+                    <ul className="catlist2">
                 <h3>Filters</h3>
                 <div>
                     <SatisfiedConsumersButton triggerYesOption={triggerYesOption}/>
